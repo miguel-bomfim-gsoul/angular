54e8f2342d6c961dc29da4bb36f8816b65b750ac
miguel-bomfim-gsoul <miguel.bomfim@gsoul.com.br>
2025-05-20 17:07:35 -0300

    Class number 42

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import { NgFor } from '@angular/common';
 import { HeaderComponent } from './header/header.component';
 import { TasksComponent } from './tasks/tasks.component';
 import { UserComponent } from './user/user.component';
@@ -7,7 +6,7 @@ import { DUMMY_USERS } from './dummy-users';
 
 @Component({
     selector: 'app-root',
-    imports: [HeaderComponent, UserComponent, TasksComponent, NgFor],
+    imports: [HeaderComponent, UserComponent, TasksComponent],
     templateUrl: './app.component.html',
     styleUrl: './app.component.css',
 })
